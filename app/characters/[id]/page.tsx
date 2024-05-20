74d50d54e25d6f5f843b6203e097da6734edbbb3
0omanuo0 <manu365manu@gmail.com>
2024-05-20 09:53:54 +0000

    Revert "feat: Add ToastContainer for displaying toast notifications"
    This reverts commit 50e5894ddc921ff55f2328874873e54487855438.

--- app/characters/[id]/page.tsx
+++ app/characters/[id]/page.tsx
@@ -3,8 +3,6 @@ import Sidebar from "@/components/sidebar"
 import { User } from "@/lib/types";
 import { useSession } from "next-auth/react";
 import { Suspense, useEffect, useState } from "react"
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 
 export default function CharacterHomepage({ params }: { params: { id: string } }) {
     const id = params.id;
@@ -14,20 +12,6 @@ export default function CharacterHomepage({ params }: { params: { id: string } }
                 <Sidebar />
             </Suspense>
             <main className=" ml-64">
-                <ToastContainer
-                    position="top-right"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                />
-                {/* Same as */}
-                {/* <ToastContainer /> */}
                 <CharacterCard id={id} />
             </main>
         </>
